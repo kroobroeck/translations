161cf82025eb074888751334337bd5a1c0a94768
Klaas Roobroeck <klaas.roobroeck@netlog.com>
2013-12-05 16:31:50 +0100

    Finished stable version and tested

--- translationtool/src/com/kroobroeck/Main.java
+++ translationtool/src/com/kroobroeck/Main.java
@@ -21,88 +21,101 @@ public class Main
 		HashMap<String, List<Inspect>> originMap = new HashMap<String, List<Inspect>>();
 		HashMap<String, List<Inspect>> localMap = new HashMap<String, List<Inspect>>();
 
-		scanTranslations( originMap, commandLineOptions.origin );
-		scanTranslations( localMap, commandLineOptions.local );
-
-		//Scan for wrong formatting of local file (%% or ||)
-		scanForWrongFormatting( localMap, commandLineOptions.local );
-
-		boolean localHasError = false;
-		boolean originHasError = false;
-
-		//Cross evaluation, check for errors
-		for ( String key : originMap.keySet() )
+		if ( commandLineOptions.origin != null )
 		{
-			if ( originMap.get( key ).size() > 2 )
-			{
-				localHasError = true;
+			scanTranslations( originMap, commandLineOptions.origin );
+			scanTranslations( localMap, commandLineOptions.local );
 
-				System.out.println( "File " + commandLineOptions.origin );
-				System.out.println( "\tContains " + originMap.get( key ).size() + " name " + key );
-				System.out.println( "\tDetails:" );
-				for ( Inspect inspect : originMap.get( key ) )
+			//Scan for wrong formatting of local file (%% or ||)
+			scanForWrongFormatting( localMap, commandLineOptions.local );
+
+			boolean localHasError = false;
+			boolean originHasError = false;
+
+			//Cross evaluation, check for errors
+			for ( String key : originMap.keySet() )
+			{
+				if ( originMap.get( key ).size() > 2 )
 				{
-					System.out.println( "\t\t" + inspect.toString() );
+					localHasError = true;
+
+					System.out.println( "File " + commandLineOptions.origin );
+					System.out.println( "\tContains " + originMap.get( key ).size() + " name " + key );
+					System.out.println( "\tDetails:" );
+					for ( Inspect inspect : originMap.get( key ) )
+					{
+						System.out.println( "\t\t" + inspect.toString() );
+					}
+					System.out.print( "\n" );
 				}
-				System.out.print( "\n" );
-			}
-			if ( !localMap.containsKey( key ) )
-			{
-				localHasError = true;
-
-				System.out.println( "File " + commandLineOptions.local );
-				System.out.println( "\tDoesn't contain " + key );
-				System.out.println( "\tLinenumber " + originMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.origin + "\n" );
-
-				if ( commandLineOptions.sync )
+				if ( !localMap.containsKey( key ) )
 				{
-					//If sync is on, write to localizations file
-					localMap.put( key, originMap.get( key ) );
-					localMap.get( key ).get( 0 ).value = "%%" + localMap.get( key ).get( 0 ).value + "%%";
+					localHasError = true;
+
+					System.out.println( "File " + commandLineOptions.local );
+					System.out.println( "\tDoesn't contain " + key );
+					System.out.println( "\tLinenumber " + originMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.origin + "\n" );
+
+					if ( commandLineOptions.sync )
+					{
+						//If sync is on, write to localizations file
+						localMap.put( key, originMap.get( key ) );
+						localMap.get( key ).get( 0 ).value = "%%" + localMap.get( key ).get( 0 ).value + "%%";
+					}
 				}
 			}
-		}
 
-		for ( String key : localMap.keySet() )
-		{
-			if ( localMap.get( key ).size() > 2 )
+			for ( String key : localMap.keySet() )
 			{
-				originHasError = true;
-
-				System.out.println( "File " + commandLineOptions.local );
-				System.out.println( "\tContains " + localMap.get( key ).size() + " name " + key );
-				System.out.println( "\tDetails:" );
-				for ( Inspect inspect : localMap.get( key ) )
+				if ( localMap.get( key ).size() > 2 )
 				{
-					System.out.println( "\t\t" + inspect.toString() );
+					originHasError = true;
+
+					System.out.println( "File " + commandLineOptions.local );
+					System.out.println( "\tContains " + localMap.get( key ).size() + " name " + key );
+					System.out.println( "\tDetails:" );
+					for ( Inspect inspect : localMap.get( key ) )
+					{
+						System.out.println( "\t\t" + inspect.toString() );
+					}
+					System.out.print( "\n" );
 				}
-				System.out.print( "\n" );
-			}
-			if ( !originMap.containsKey( key ) )
-			{
-				originHasError = true;
+				if ( !originMap.containsKey( key ) )
+				{
+					originHasError = true;
 
-				System.out.println( "File " + commandLineOptions.origin );
-				System.out.println( "\tDoesn't contain " + key );
-				System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
+					System.out.println( "File " + commandLineOptions.origin );
+					System.out.println( "\tDoesn't contain " + key );
+					System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
+				} else if ( originMap.get( key ).get( 0 ).formatted != localMap.get( key ).get( 0 ).formatted )
+				{
+					System.out.println( "File " + commandLineOptions.origin );
+					System.out.println( "\tKey " + key + " doesn't match the formatted value" + localMap.get( key ).get( 0 ).formatted.getTag() + "\n\tin " + commandLineOptions.local );
+					System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
+				}
 			}
-			else if ( originMap.get(key).get( 0 ).formatted != localMap.get(key).get( 0 ).formatted )
-			{
-				System.out.println( "File " + commandLineOptions.origin );
-				System.out.println( "\tKey " + key + " doesn't match the formatted value"  + localMap.get(key).get( 0 ).formatted.getTag() + "\n\tin " + commandLineOptions.local );
-				System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
-			}
-		}
 
-		//If no errors occured and --format is set, middle is reformatted.
-		if ( !localHasError && !originHasError && commandLineOptions.format )
+			//If no errors occured and --format is set, middle is reformatted.
+			if ( !localHasError && !originHasError && commandLineOptions.format )
+			{
+				System.out.println( "The newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
+				sortLocalizations( localMap, commandLineOptions.local );
+			} else if ( localHasError && commandLineOptions.sync )
+			{
+				System.out.println( "Synced localization with orginal, newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
+				sortLocalizations( localMap, commandLineOptions.local );
+			}
+		} else
 		{
-			System.out.println( "The newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
-			sortLocalizations( localMap, commandLineOptions.local );
-		} else if ( localHasError && commandLineOptions.sync )
-		{
-			System.out.println( "Synced localization with orginal, newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
-			sortLocalizations( localMap, commandLineOptions.local );
+
+			scanTranslations( localMap, commandLineOptions.local );
+			//Scan for wrong formatting of local file (%% or ||)
+			scanForWrongFormatting( localMap, commandLineOptions.local );
+
+			if ( commandLineOptions.format )
+			{
+				sortLocalizations( localMap, commandLineOptions.local );
+			}
 		}
 	}
 
@@ -131,7 +144,7 @@ public class Main
 				inspect.name = matcher.group( 1 ) + matcher.group( 2 );
 				inspect.value = matcher.group( 4 );
 				inspect.line = line;
-				inspect.formatted = matcher.group( 3 ).contains("false" ) ? Formatted.FALSE : matcher.group( 3 ).equals( "" ) ? Formatted.UNDEFINED : Formatted.TRUE;
+				inspect.formatted = matcher.group( 3 ).contains( "false" ) ? Formatted.FALSE : matcher.group( 3 ).equals( "" ) ? Formatted.UNDEFINED : Formatted.TRUE;
 
 				List<Inspect> inspectList = null;
 				if ( map.containsKey( inspect.name ) )
