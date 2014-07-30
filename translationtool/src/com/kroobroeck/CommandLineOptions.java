62a31c3022dc646ae0b235599eb6dfd375c00818
Klaas Roobroeck <klaasroobroeck@gmail.com>
2014-07-30 13:45:51 +0200

    Make translations jar file to cope only with sorting of strings.xml. Al other things which were previously there can be done with lint

--- translationtool/src/com/kroobroeck/CommandLineOptions.java
+++ translationtool/src/com/kroobroeck/CommandLineOptions.java
@@ -17,15 +17,9 @@ public class CommandLineOptions
 	@Parameter
 	public List<String> parameters = new ArrayList<String>();
 
-	@Parameter(names = {"--format", "-f"}, description = "Format the middle strings.xml file.")
-	public boolean format = false;
-
 	@Parameter(names = {"--origin", "-o"}, description = "The path to the original strings.xml file")
 	public String origin;
 
-	@Parameter(names = {"--local", "-l"}, description = "The path to the local strings.xml file")
-	public String local;
-
-	@Parameter(names = {"--sync", "-s"}, description = "Sync the origin with the local file when certain tags aren't available in the local strings.xml")
-	public boolean sync = false;
+	@Parameter(names = {"--sync", "-s"}, description = "Sort the xml file according to the string names")
+	public boolean sort = false;
 }
--- translationtool/src/com/kroobroeck/Main.java
+++ translationtool/src/com/kroobroeck/Main.java
@@ -14,108 +14,17 @@ public class Main
 {
 	public static void main( String[] args ) throws Exception
 	{
-		//Parse commands
 		CommandLineOptions commandLineOptions = new CommandLineOptions();
 		new JCommander( commandLineOptions, args );
 
 		HashMap<String, List<Inspect>> originMap = new HashMap<String, List<Inspect>>();
-		HashMap<String, List<Inspect>> localMap = new HashMap<String, List<Inspect>>();
-
 		if ( commandLineOptions.origin != null )
 		{
 			scanTranslations( originMap, commandLineOptions.origin );
-			scanTranslations( localMap, commandLineOptions.local );
-
-			//Scan for wrong formatting of local file (%% or ||)
-			scanForWrongFormatting( localMap, commandLineOptions.local );
-
-			boolean localHasError = false;
-			boolean originHasError = false;
-
-			//Cross evaluation, check for errors
-			for ( String key : originMap.keySet() )
-			{
-				if ( originMap.get( key ).size() > 2 )
-				{
-					localHasError = true;
-
-					System.out.println( "File " + commandLineOptions.origin );
-					System.out.println( "\tContains " + originMap.get( key ).size() + " name " + key );
-					System.out.println( "\tDetails:" );
-					for ( Inspect inspect : originMap.get( key ) )
-					{
-						System.out.println( "\t\t" + inspect.toString() );
-					}
-					System.out.print( "\n" );
-				}
-				if ( !localMap.containsKey( key ) )
-				{
-					localHasError = true;
-
-					System.out.println( "File " + commandLineOptions.local );
-					System.out.println( "\tDoesn't contain " + key );
-					System.out.println( "\tLinenumber " + originMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.origin + "\n" );
-
-					if ( commandLineOptions.sync )
-					{
-						//If sync is on, write to localizations file
-						localMap.put( key, originMap.get( key ) );
-						localMap.get( key ).get( 0 ).value = "%%" + localMap.get( key ).get( 0 ).value + "%%";
-					}
-				}
-			}
-
-			for ( String key : localMap.keySet() )
-			{
-				if ( localMap.get( key ).size() > 2 )
-				{
-					originHasError = true;
-
-					System.out.println( "File " + commandLineOptions.local );
-					System.out.println( "\tContains " + localMap.get( key ).size() + " name " + key );
-					System.out.println( "\tDetails:" );
-					for ( Inspect inspect : localMap.get( key ) )
-					{
-						System.out.println( "\t\t" + inspect.toString() );
-					}
-					System.out.print( "\n" );
-				}
-				if ( !originMap.containsKey( key ) )
-				{
-					originHasError = true;
-
-					System.out.println( "File " + commandLineOptions.origin );
-					System.out.println( "\tDoesn't contain " + key );
-					System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
-				} else if ( originMap.get( key ).get( 0 ).formatted != localMap.get( key ).get( 0 ).formatted )
-				{
-					System.out.println( "File " + commandLineOptions.origin );
-					System.out.println( "\tKey " + key + " doesn't match the formatted value" + localMap.get( key ).get( 0 ).formatted.getTag() + "\n\tin " + commandLineOptions.local );
-					System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + commandLineOptions.local + "\n" );
-				}
-			}
-
-			//If no errors occured and --format is set, middle is reformatted.
-			if ( !localHasError && !originHasError && commandLineOptions.format )
-			{
-				System.out.println( "The newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
-				sortLocalizations( localMap, commandLineOptions.local );
-			} else if ( localHasError && commandLineOptions.sync )
-			{
-				System.out.println( "Synced localization with orginal, newly sorted localizations file can be found " + commandLineOptions.local + "\n" );
-				sortLocalizations( localMap, commandLineOptions.local );
-			}
-		} else
-		{
-
-			scanTranslations( localMap, commandLineOptions.local );
-			//Scan for wrong formatting of local file (%% or ||)
-			scanForWrongFormatting( localMap, commandLineOptions.local );
-
-			if ( commandLineOptions.format )
-			{
-				sortLocalizations( localMap, commandLineOptions.local );
-			}
+            
+            if( commandLineOptions.sort ) {
+                sortLocalizations(originMap, commandLineOptions.origin);
+            }
 		}
 	}
 
@@ -183,22 +92,5 @@ public class Main
 
 		writer.println( "</resources>" );
 		writer.close();
-	}
-
-	private static void scanForWrongFormatting( HashMap<String, List<Inspect>> localMap, String local )
-	{
-		Pattern pattern = Pattern.compile( "(^%%(.*?)%%)|(^\\|\\|(.*?)\\|\\|)" );
-
-		for ( String key : localMap.keySet() )
-		{
-			Matcher matcher = pattern.matcher( (localMap.get( key ).get( 0 ).value) );
-
-			if ( !matcher.find() )
-			{
-				System.out.println( "File " + local );
-				System.out.println( "\tThe value of " + key + " has wrong formatting (no %% or || at beginning and/or end)" );
-				System.out.println( "\tLinenumber " + localMap.get( key ).get( 0 ).lineNumber + " in " + local + "\n" );
-			}
-		}
 	}
 }
